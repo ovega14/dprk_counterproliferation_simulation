1dd628554a0ebe65921ce91f031153f04b37f984
Octavio Vega <66711602+ovega14@users.noreply.github.com>
2022-05-06 20:00:19 -0400

    Add files via upload

---
+++ armaments.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Massive ordinance penetrator
+class MOP():
+	def __init__(self, *args):
+
+		self.name = 'MOP'
+		self.weight = 3*10**4 # weight in pounds
+		self.depth = 100 # penetration depth in feet
+
+	def penetrate():
+		self.depth + np.random.normal(0,5)
+
